241003008bd8ec3cc3ad3ab9a6e200b313899f0b
porridgewithraisins <santhanambr2002@gmail.com>
2022-12-17 18:11:42 +0530

    add prizes

--- data/migration.sql
+++ data/migration.sql
@@ -2,7 +2,7 @@ CREATE TABLE
     IF NOT EXISTS users(
         username TEXT UNIQUE NOT NULL,
         email TEXT PRIMARY KEY,
-        password TEXT,
+        password TEXT NOT NULL,
         level_reached_at TIMESTAMP DEFAULT (strftime('%s', 'now')),
         level INTEGER DEFAULT 1
     );
